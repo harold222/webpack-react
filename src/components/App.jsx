5a1d77d7612e0c0bfe388081cddd0e362d56093c
Harold Pedraza <32584758+harold222@users.noreply.github.com>
2023-03-26 08:12:32 -0500

    with babel

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+ *
+ * @returns {JSX.Element} Componente principal de nuestra aplicación
+ */
+const App = () => {
+    return (
+        <div>
+            <h1>React</h1>
+            <ul>
+                <li>JSX</li>
+            </ul>
+        </div>
+    );
+};
+
+export default App;
